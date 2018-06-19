fbeef50d42f8a741bfdd7d8836c22449a0cb4148
feifeiyuan <33796779+feifeiyuan@users.noreply.github.com>
2018-06-19 16:47:01 +0800

    cpu_loading脚本计算

--- python/time_in_state计算cpu_loading/get_statistic.bat
+++ python/time_in_state计算cpu_loading/get_statistic.bat
@@ -3,6 +3,6 @@ if "%test_times%"=="" set test_times=60
 set dir=%cd%\tool\cpu_data.sh 
 adb push %dir% /sdcard/
 adb shell sh /sdcard/cpu_data.sh %test_times%
-adb pull /sdcard/result	%cd%\tool\
+adb pull /sdcard/cpu_loading	%cd%\tool\
 
 pause
--- python/time_in_state计算cpu_loading/setup.bat
+++ python/time_in_state计算cpu_loading/setup.bat
@@ -3,7 +3,7 @@ if "%test_times%"=="" set test_times=60
 set dir=%cd%\tool\cpu_data.sh 
 adb push %dir% /sdcard/
 adb shell sh /sdcard/cpu_data.sh %test_times%
-adb pull /sdcard/result/	%cd%\tool\
+adb pull /sdcard/cpu_loading/	%cd%\tool\
 
 python %cd%\tool\cpu_data_compute.py %cd%\tool\ > %cd%\result.txt
 pause
