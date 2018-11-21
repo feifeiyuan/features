b78805adc691c7f16589fb3319ca22bb89b73c55
feifeiyuan <33796779+feifeiyuan@users.noreply.github.com>
2018-11-21 10:50:26 +0800

    cpufreq和cpuidle计算cpu_loading

--- C/cpufreq和idle计算cpu_loading/cpu_loading.c
+++ C/cpufreq和idle计算cpu_loading/cpu_loading.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <errno.h>  
-#include <unistd.h>  
-#include <sys/time.h>  
+#include <errno.h>
+#include <unistd.h>
+#include <sys/time.h>
 #include<unistd.h>
 #include <fnmatch.h>
 #include <sys/types.h>
@@ -11,42 +11,41 @@
 
 #include "cpu_loading.h"
 
-static si get_argv(char *time_char)  
-{  
-    char *ch = time_char;  
-    si ret = 0;  
-      
-    if( *ch == '0'){  
-        fprintf(stderr, "interval should not be zero\n");  
-        return INVALID_ARGV;  
-    }  
-  
-    for(ch=time_char; *ch; ch++){  
-        if( *ch == '-' || !(*ch >= '0' || *ch <= '9')){  
-            fprintf(stderr, "please input the correct argv time(no negative)\n");  
-            return INVALID_ARGV;  
-        }  
-        ret = ret*10+(*ch-'0');  
-    }  
-    return ret;  
-}  
+static si get_argv(char *time_char)
+{
+	char *ch = time_char;
+	si ret = 0;
+
+	if( *ch == '0'){
+		fprintf(stderr, "interval should not be zero\n");
+		return INVALID_ARGV;
+	}
+
+	for(ch=time_char; *ch; ch++){
+		if( *ch == '-' || !(*ch >= '0' || *ch <= '9')){
+			fprintf(stderr, "please input the correct argv time(no negative)\n");
+			return INVALID_ARGV;
+		}
+		ret = ret*10+(*ch-'0');
+	}
+	return ret;
+}
 
 static si check_argv(char *tag, si *accummulate_time, char *value)
 {
 	si argv_time = *accummulate_time;
-	//printf("%s\n",tag);
-	//printf("%d\n",strcmp(tag,"-i"));
 	if(strcmp(tag,"-t")==0){
-		argv_time = get_argv(value); 
+		argv_time = get_argv(value);
 		if(argv_time<0)
-			return INVALID_ARGV;    			 
+			return INVALID_ARGV;
 	}else{
-		fprintf(stderr, "you must input correct argvs\n");   
-		fprintf(stderr, "expl: ./data/local/tmp/cpu_sgm -i 100(ms) -t 60(s)\n");  
-		return INVALID_ARGV; 
+		fprintf(stderr, "you must input correct argvs\n");
+		fprintf(stderr, "expl: ./cpu_loading -t 60(s)\n");
+		return INVALID_ARGV;
 	}
-	if(argv_time==0)
+	if(argv_time==0){
 		argv_time = DEFAULT_TIME;
+	}
 	*accummulate_time = argv_time;
 	return 0;
 }
@@ -76,25 +75,23 @@ static si get_processor_num()
 	return cpu_num;
 }
 
-static s8 get_init_freq_count(const char *cpu_cur_path, u8 *size, ui *cpu_available_freq)  
-{  
-    FILE    *fp = NULL;  
+static s8 get_init_freq_count(const char *cpu_cur_path, u8 *size, ui *cpu_available_freq)
+{
+	FILE    *fp = NULL;
 	memset(cpu_available_freq, 0, MAX_AVALLABLE_FREQ);
-    fp = fopen(cpu_cur_path, "r");  
-    u8 i = 0;  
-    if(fp==NULL){  
-        fprintf(stderr, "failed opend file:", cpu_cur_path);  
-        return INVALID_ARGV;  
-    }  
-    // even freq     
-    // odd count  
-    for(i=0; i<MAX_AVALLABLE_FREQ; i++){
-        if(fscanf(fp,"%d", cpu_available_freq+i)!=1)  
-            break;  
-    }  
-    *size = i;  
-    fclose(fp);  
-    return 0;  
+	fp = fopen(cpu_cur_path, "r");
+	u8 i = 0;
+	if(fp==NULL){
+		fprintf(stderr, "%s:failed opend file:%s", __func__, cpu_cur_path);
+		return INVALID_ARGV;
+	}
+	for(i=0; i<MAX_AVALLABLE_FREQ; i++){
+		if(fscanf(fp,"%d", cpu_available_freq+i)!=1)
+			break;
+	}
+	*size = i;
+	fclose(fp);
+	return 0;
 }
 
 static int cal_state_time(u8 cpu0_size, u8 cpu4_size, ui *cpu0_available_freq_start, \
@@ -116,7 +113,7 @@ ui *cpu4_available_freq_start, ui *cpu0_available_freq_end, ui *cpu4_available_f
 	}
 	*lit_total_time = lit_time;
 	*big_total_time = big_time;
-	
+
 	return 0;
 }
 
@@ -162,7 +159,7 @@ static long long int get_idle_time(int cpu, char *state)
 	strcat(path, state);
 	strcat(path, "/time");
 	//printf("%s\n", path);
-	
+
 	FILE *fp = NULL;
 	long long int value = 0;
 	fp = fopen(path, "r");
@@ -183,7 +180,7 @@ static int get_cpu_online(int cpu)
 	sprintf(str, "%d", cpu);
 	strcat(buffer, str);
 	strcat(buffer, "/online");
-	
+
 	FILE *fp = NULL;
 	int value = 0;
 	fp = fopen(buffer, "r");
@@ -229,7 +226,7 @@ static double dis_result(u8 cpu_size, ui *cpu_available_freq_start, int start, i
 			printf("%.2f%\t%.2f%\n", active_per*100, (1-active_per)*100);
 		}
 	}
-	printf("\t");
+	printf("freq\t");
 	for(i=0; i<cpu_size; i=i+2){
 		float freq_perc = cpu_available_freq_start[i+1]*1.0/temp_total_time;
 		printf("%.2f%\t", freq_perc*100);
@@ -238,14 +235,14 @@ static double dis_result(u8 cpu_size, ui *cpu_available_freq_start, int start, i
 	for(i=0; i<cpu_size; i=i+2){
 		sum += (sum_arr[i+1]*cpu_available_freq_start[i]);
 	}
-	printf("\nsum:%.2f\n\n", sum);
+	printf("\nsum:%.2fkHZ\n\n", sum);
 	return sum;
 }
 
 static int get_cpu_loading(si accummulate_time, si processor_num)
 {
 	u8   cpu0_size, cpu4_size;
-	ui   cpu0_available_freq_start[MAX_AVALLABLE_FREQ], cpu4_available_freq_start[MAX_AVALLABLE_FREQ];  
+	ui   cpu0_available_freq_start[MAX_AVALLABLE_FREQ], cpu4_available_freq_start[MAX_AVALLABLE_FREQ];
 	ui   cpu0_available_freq_end[MAX_AVALLABLE_FREQ], cpu4_available_freq_end[MAX_AVALLABLE_FREQ];
 	int  i = 0;
 	if(get_init_freq_count(POLICY0_TIME_STATS, &cpu0_size, cpu0_available_freq_start)<0){
@@ -258,7 +255,7 @@ static int get_cpu_loading(si accummulate_time, si processor_num)
 		cpu_idle_state1_start[i] = get_idle_time(i, "state1");
 		//printf("%ld\t%ld\n", cpu_idle_state0_start[i], cpu_idle_state1_start[i]);
 	}
-	
+
 	if(processor_num>LIT_CLUSTER_END){
 		if(get_init_freq_count(POLICY4_TIME_STATS, &cpu4_size, cpu4_available_freq_start)<0)
 			return ERROR;
@@ -279,7 +276,7 @@ static int get_cpu_loading(si accummulate_time, si processor_num)
 		cpu_idle_state1_end[i] = get_idle_time(i, "state1");
 		//printf("%ld\t%ld\n", cpu_idle_state0_end[i], cpu_idle_state1_end[i]);
 	}
-	
+
 	if(processor_num>LIT_CLUSTER_END){
 		if(get_init_freq_count(POLICY4_TIME_STATS, &cpu4_size, cpu4_available_freq_end)<0)
 			return ERROR;
@@ -292,16 +289,16 @@ static int get_cpu_loading(si accummulate_time, si processor_num)
 	uli lit_total_time = 0, big_total_time = 0;
 	cal_state_time(cpu0_size, cpu4_size, cpu0_available_freq_start, \
 	cpu4_available_freq_start, cpu0_available_freq_end, cpu4_available_freq_end, processor_num, &lit_total_time, &big_total_time);
-	
+
 	//printf("lit_total_time:%ld\n", lit_total_time);
 	//printf("big_total_time:%ld\n", big_total_time);
 	cal_idle_time(cpu_idle_state0_start, cpu_idle_state1_start, cpu_idle_state0_end, cpu_idle_state1_end, processor_num);
-	
+
 	double sum_lit = dis_result(cpu0_size, cpu0_available_freq_start, 0, LIT_CLUSTER_END, lit_total_time, cpu_idle_state0_start);
 
 	if(processor_num>LIT_CLUSTER_END){
 		double sum_big = dis_result(cpu4_size, cpu4_available_freq_start, LIT_CLUSTER_END, processor_num, big_total_time, cpu_idle_state0_start);
-		printf("SUM:%.2f\n", sum_lit+sum_big);
+		printf("SUM:%.2fkHZ\n", sum_lit+sum_big);
 	}
 	return 0;
 }
@@ -310,36 +307,37 @@ int main(int argc, char *argv[])
 {
 	si   accummulate_time = 0;
 	si 	 processor_num = 0;
-	
-	
+
+
 	if(getuid()!=0){
 		fprintf(stderr, "You should download userdebug and su root Firstly\n");
 		return NOT_ROOT;
 	}
-	if( argc < 1 ){  
-        fprintf(stderr, "you must input at least one argv");   
-        fprintf(stderr, "expl: ./data/local/tmp/cpu_loading -t 60(s)\n");  
-        return INVALID_ARGV;  
-    }  
+	if( argc < 1 ){
+		fprintf(stderr, "you must input at least one argv");
+		fprintf(stderr, "expl: ./cpu_loading -t 60(s)\n");
+		return INVALID_ARGV;
+	}
 	if( argc%2!=1){
-		fprintf(stderr, "you must input odd number argvs");   
-		fprintf(stderr, "expl: ./data/local/tmp/cpu_loading -t 60(s)\n");  
-        return INVALID_ARGV;  
-	} 
-	// input argc 1  
-    if(argc==1){  
-        accummulate_time = DEFAULT_TIME;  		
-    // input argc 3  or more
-    }else if(argc==3){
+		fprintf(stderr, "you must input odd number argvs");
+		fprintf(stderr, "expl: ./cpu_loading -t 60(s)\n");
+		return INVALID_ARGV;
+	}
+
+	if(argc==1){
+		accummulate_time = DEFAULT_TIME;
+	}else if(argc==3){
 		if(check_argv(argv[1], &accummulate_time, argv[2])<0)
 			return INVALID_ARGV;
-    }
-	printf("accummulate_time is %d(s)\n", accummulate_time); 
-	
+	}
+	printf("accummulate_time is %d(s)\n", accummulate_time);
+
 	processor_num = get_processor_num();
 	if(processor_num<=0){
 		return ERROR;
 	}
-	if(get_cpu_loading(accummulate_time, processor_num)<0)
+	if(get_cpu_loading(accummulate_time, processor_num)<0){
 		return ERROR;
+	}
+	return 0;
 }
--- C/cpufreq和idle计算cpu_loading/cpu_loading.h
+++ C/cpufreq和idle计算cpu_loading/cpu_loading.h
@@ -1,23 +1,23 @@
-typedef signed      int    si;  
-typedef signed      char   s8;  
-typedef unsigned    char   u8;  
-typedef unsigned    int    ui;  
-typedef unsigned long int  uli;  
+typedef signed      int    si;
+typedef signed      char   s8;
+typedef unsigned    char   u8;
+typedef unsigned    int    ui;
+typedef unsigned long int  uli;
 typedef unsigned long long ulli;
 
-#define INVALID_ARGV        -1  
-#define FAILED_OPEN_FILE    -2  
-#define ERROR				-22
-#define NOT_ROOT			-4
+#define INVALID_ARGV        -1
+#define FAILED_OPEN_FILE    -2
+#define ERROR		    -22
+#define NOT_ROOT	    -4
 
-#define BUFFER_SIZE			256
-#define SIZE				20
-#define MAX_AVALLABLE_FREQ  40 
-#define LIT_CLUSTER_END		4
-#define DEFAULT_TIME		60
+#define BUFFER_SIZE	    256
+#define SIZE		    20
+#define MAX_AVALLABLE_FREQ  40
+#define LIT_CLUSTER_END	    4
+#define DEFAULT_TIME	    60
 
-#define CPU0_AVAILABLE_FREQ_PATH    "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies"  
-#define CPU4_AVAILABLE_FREQ_PATH    "/sys/devices/system/cpu/cpu4/cpufreq/scaling_available_frequencies" 
+#define CPU0_AVAILABLE_FREQ_PATH    "/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies"
+#define CPU4_AVAILABLE_FREQ_PATH    "/sys/devices/system/cpu/cpu4/cpufreq/scaling_available_frequencies"
 
-#define POLICY0_TIME_STATS			"/sys/devices/system/cpu/cpufreq/policy0/stats/time_in_state"
-#define POLICY4_TIME_STATS			"/sys/devices/system/cpu/cpufreq/policy4/stats/time_in_state"
+#define POLICY0_TIME_STATS	    "/sys/devices/system/cpu/cpufreq/policy0/stats/time_in_state"
+#define POLICY4_TIME_STATS	    "/sys/devices/system/cpu/cpufreq/policy4/stats/time_in_state"
